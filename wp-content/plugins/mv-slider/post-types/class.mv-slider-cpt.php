2673dce9d76cfc558e6bee951a62ba393e31f07d
d1av <davi4alves@gmail.com>
2023-05-28 13:22:13 -0300

    feat: :sparkles: add a new metabox

--- wp-content/plugins/mv-slider/post-types/class.mv-slider-cpt.php
+++ wp-content/plugins/mv-slider/post-types/class.mv-slider-cpt.php
@@ -6,6 +6,7 @@ if (!class_exists('MV_Slider_Post_Type')) {
     function __construct()
     {
       add_action('init', array($this, 'create_post_type'));
+      add_action('add_meta_boxes', array($this, 'add_meta_boxes'));
     }
 
     public function create_post_type()
@@ -26,12 +27,29 @@ if (!class_exists('MV_Slider_Post_Type')) {
         'show_in_admin_bar' => true,
         'show_in_nav_menus' => true,
         'can_export' =>  true,
-        'has_archive'=> false,
+        'has_archive' => false,
         'exclude_from_search' => false,
         'public_queryable' => true,
         'show_in_rest' => true,
-        'menu_icon' => 'dashicons-images-alt2'
+        'menu_icon' => 'dashicons-images-alt2',
+        'register_meta_box_cb' => array($this, 'add_meta_boxes'),
       ));
+    }
+
+    public function add_meta_boxes()
+    {
+      add_meta_box(
+        'mv_slider_meta_box',
+        'Link Options',
+        array($this,'add_inner_meta_boxes'),
+        'mv-slider',
+        'advanced',
+        'high'
+      );
+    }
+
+    public function add_inner_meta_boxes($post){
+
     }
   }
 }
