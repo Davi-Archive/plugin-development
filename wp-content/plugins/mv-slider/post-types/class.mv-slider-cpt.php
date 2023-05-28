bd982c3d2ad1fd69f178c744ba0fc4cc7cfed895
d1av <davi4alves@gmail.com>
2023-05-28 14:57:57 -0300

    feat: :sparkles: add metabox sanitization

--- wp-content/plugins/mv-slider/post-types/class.mv-slider-cpt.php
+++ wp-content/plugins/mv-slider/post-types/class.mv-slider-cpt.php
@@ -56,11 +56,41 @@ if (!class_exists('MV_Slider_Post_Type')) {
 
     public function save_post($post_id)
     {
+      if (isset($_POST['mv_slider_nonce'])) {
+        if (!wp_verify_nonce($_POST['mv_slider_nonce'], 'mv_slider_nonce')) {
+          return;
+        }
+      }
+
+      if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) {
+        return;
+      }
+
+      if (isset($_POST['post_type']) && $_POST['post_type'] === 'mv-slider') {
+        if (!current_user_can('edit_page', $post_id)) {
+          return;
+        } elseif (!current_user_can('edit_post', $post_id)) {
+          return;
+        }
+      }
+
       if (isset($_POST['action']) && $_POST['action'] == 'editpost') {
         $old_link_text = get_post_meta($post_id, 'mv_slider_link_text', true);
-        $new_link_text = $_POST['mv_slider_link_text'];
+        $new_link_text = sanitize_text_field($_POST['mv_slider_link_text']);
         $old_link_url = get_post_meta($post_id, 'mv_slider_link_url', true);
-        $new_link_url = $_POST['mv_slider_link_url'];
+        $new_link_url = esc_url_raw($_POST['mv_slider_link_url']);
+
+        if (empty($new_link_text)) {
+          update_post_meta($post_id, 'mv_slider_link_text', 'Add some text');
+        } else {
+          update_post_meta($post_id, 'mv_slider_link_text', $new_link_text, $old_link_text);
+        }
+
+        if (empty($new_link_url)) {
+          update_post_meta($post_id, 'mv_slider_link_url', '#');
+        } else {
+          update_post_meta($post_id, 'mv_slider_link_url', $new_link_url, $old_link_url);
+        }
 
         update_post_meta($post_id, 'mv_slider_link_text', $new_link_text, $old_link_text);
         update_post_meta($post_id, 'mv_slider_link_url', $new_link_url, $old_link_url);
--- wp-content/plugins/mv-slider/views/mv-slider_metabox.php
+++ wp-content/plugins/mv-slider/views/mv-slider_metabox.php
@@ -1,10 +1,20 @@
+<pre>
+<?php
+$meta = get_post_meta($post->ID);
+$link_text = get_post_meta($post->ID, 'mv_slider_link_text', true);
+$link_url = get_post_meta($post->ID, 'mv_slider_link_url', true);
+?>
+</pre>
+
 <table class="form-table mv-slider-metabox">
+  <input type="hidden" name="mv_slider_nonce" value="<?php echo wp_create_nonce('mv_slider_nonce'); ?>" />
   <tr>
     <th>
       <label for="mv_slider_link_text">Link Text</label>
     </th>
     <td>
-      <input type="text" name="mv_slider_link_text" id="mv_slider_link_text" class="regular-text link-text" value="" required />
+      <input type="text" name="mv_slider_link_text" id="mv_slider_link_text" class="regular-text link-text" value="<?=
+                                                                                                                    isset($link_text) ? esc_html($link_text) : '' ?>" required />
     </td>
   </tr>
   <tr>
@@ -12,7 +22,10 @@
       <label for="mv_slider_link_text">Link URL</label>
     </th>
     <td>
-      <input type="urç" name="mv_slider_link_url" id="mv_slider_link_url" class="regular-text link-url" value="" required />
+      <input type="urç" name="mv_slider_link_url" id="mv_slider_link_url" class="regular-text link-url" value="<?=
+                                                                                                                isset($meta['mv_slider_link_url'][0]) ? esc_url(
+                                                                                                                  $meta['mv_slider_link_url'][0]
+                                                                                                                ) : ''; ?>" required />
     </td>
   </tr>
 </table>
