a35fc7cf8b3384f30c021d2c02d94498500b6000
d1av <davi4alves@gmail.com>
2023-05-28 02:59:03 -0300

    feat: :sparkles: add plugin name

---
+++ wp-content/plugins/mv-slider/mv-slider.php
@@ -0,0 +1,31 @@
+<?php
+/*
+* Plugin Name: MV Slider
+* Plugin URI: http://www.wordpress.org/mv-slider/
+* Version: 1.0
+* Requires at least: 8.0
+* Author: Davi
+* Author URI: http://www.google.com/
+* License: GPL v2 or later
+* License URI: http://www.gnu.org/licenses/gpl-2.0.html
+* Text Domain: mv-slider
+* Domain Path: /languages/
+*/
+
+// MV Slider is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// MV Slider is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with MV Slider. If not, see < http://www.gnu.org/licenses/ >.
+
+if (!defined('ABSPATH')) {
+ // die('No direct script access');  // message displayed on direct access
+  exit;
+}
