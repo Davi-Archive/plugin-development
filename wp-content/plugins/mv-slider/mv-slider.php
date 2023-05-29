863971408168d2e98757beeffd5534a56f47cad1
d1av <davi4alves@gmail.com>
2023-05-29 18:16:18 -0300

    feat: :sparkles: add uninstall

--- wp-content/plugins/mv-slider/mv-slider.php
+++ wp-content/plugins/mv-slider/mv-slider.php
@@ -78,6 +78,19 @@ if (!class_exists('MV _Slider')) {
 
     public static function uninstall()
     {
+      delete_option('mv_slider_options');
+
+      $posts = get_posts(
+        array(
+          'post_type' => 'mv-slider',
+          'number_posts' => -1,
+          'post_status' => 'any'
+        )
+      );
+
+      foreach ($posts as $post) {
+        wp_delete_post($post->ID, true);
+      }
     }
 
     public function add_menu()
