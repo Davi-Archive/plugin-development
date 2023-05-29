cd7ce487acc6d2f688109e17bb7f4d50faa2c326
d1av <davi4alves@gmail.com>
2023-05-29 03:42:33 -0300

    feat: :sparkles: add slides to frontend

--- wp-content/plugins/mv-slider/views/mv-slider_shortcode.php
+++ wp-content/plugins/mv-slider/views/mv-slider_shortcode.php
@@ -6,29 +6,36 @@
     <?php
     $args = array(
       'post_type' => 'mv-slider',
-      'post_status' => 'published',
-      'post__in'=> $id,
-      'orderby'=> $orderby
+      'post_status' => 'publish',
+      'post__in' => $id,
+      'orderby' => $orderby
     );
 
     $my_query = new WP_Query($args);
-    
+
+    if ($my_query->have_posts()) : while ($my_query->have_posts()) : $my_query->the_post();
+
+        $button_text = get_post_meta(get_the_ID(), 'mv_slider_link_text', true);
+        $button_url = get_post_meta(get_the_ID(), 'mv_slider_link_url', true);
     ?>
-    <li>
-      <div class="mvs-container">
-        <div class="slider-details-container">
-          <div class="wrapper">
-            <div class="slider-title">
-              <h2>Slider Title</h2>
-            </div>
-            <div class="slider-description">
-              <div class="subtitle">Subtitle</div>
-              <a href="#" class="link">Button Text</a>
+        <li>
+          <?php the_post_thumbnail('medium',  array('img-fluid')); ?>
+          <div class="mvs-container">
+            <div class="slider-details-container">
+              <div class="wrapper">
+                <div class="slider-title">
+                  <h2><?= the_title() ?></h2>
+                </div>
+                <div class="slider-description">
+                  <div class="subtitle"><?= the_content() ?></div>
+                  <a href="<?= esc_attr($button_url) ?>" class="link"><?= esc_html($button_text); ?></a>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-      </div>
-    </li>
-
+        </li>
+    <?php endwhile;
+      wp_reset_postdata();
+    else : endif; ?>
   </ul>
 </div>
