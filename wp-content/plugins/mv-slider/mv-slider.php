57ca0425b9e3fcc24bc0f5efa66056ac60b89901
d1av <davi4alves@gmail.com>
2023-05-28 23:30:03 -0300

    feat: :sparkles: add admin menu

--- wp-content/plugins/mv-slider/mv-slider.php
+++ wp-content/plugins/mv-slider/mv-slider.php
@@ -101,7 +101,7 @@ if (!class_exists('MV _Slider')) {
 
     public function mv_slider_settings_page()
     {
-      echo "This is a test page";
+      require(MV_SLIDER_PATH . 'views/settings-page.php');
     }
   }
 }
