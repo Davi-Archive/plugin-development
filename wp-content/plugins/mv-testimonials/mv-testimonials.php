e709a330bf88dd249c9f3d40aed094d260f814ae
d1av <davi4alves@gmail.com>
2023-05-29 21:27:13 -0300

    feat: :sparkles: add testimonials cpt(custom post type)

--- wp-content/plugins/mv-testimonials/mv-testimonials.php
+++ wp-content/plugins/mv-testimonials/mv-testimonials.php
@@ -1,19 +1,19 @@
 <?php
 
 /**
-* Plugin Name: MV Testimonials
-* Plugin URI: https://www.wordpress.org/mv-testimonials
-* Description: My plugin's description
-* Version: 1.0
-* Requires at least: 5.6
-* Requires PHP: 7.0
-* Author: Marcelo Vieira
-* Author URI: https://www.codewave.fun
-* License: GPL v2 or later
-* License URI: https://www.gnu.org/licenses/gpl-2.0.html
-* Text Domain: mv-testimonials
-* Domain Path: /languages
-*/
+ * Plugin Name: MV Testimonials
+ * Plugin URI: https://www.wordpress.org/mv-testimonials
+ * Description: My plugin's description
+ * Version: 1.0
+ * Requires at least: 5.6
+ * Requires PHP: 7.0
+ * Author: Marcelo Vieira
+ * Author URI: https://www.codewave.fun
+ * License: GPL v2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: mv-testimonials
+ * Domain Path: /languages
+ */
 /*
 MV Testimonials is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -29,60 +29,67 @@ You should have received a copy of the GNU General Public License
 along with MV Testimonials. If not, see https://www.gnu.org/licenses/gpl-2.0.html.
 */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly
+if (!defined('ABSPATH')) {
+    exit; // Exit if accessed directly
 }
 
-if( !class_exists( 'MV_Testimonials' ) ){
+if (!class_exists('MV_Testimonials')) {
 
-    class MV_Testimonials{
+    class MV_Testimonials
+    {
 
-        public function __construct() {
+        public function __construct()
+        {
 
             // Define constants used througout the plugin
             $this->define_constants();
 
+            require_once(MV_TESTIMONIALS_PATH . 'post-types/class.mv-testimonials-cpt.php');
+            $MVTestimonialsPostType = new MV_Testimonials_Post_Type();
         }
 
-         /**
+        /**
          * Define Constants
          */
-        public function define_constants(){
+        public function define_constants()
+        {
             // Path/URL to root of this plugin, with trailing slash.
-            define ( 'MV_TESTIMONIALS_PATH', plugin_dir_path( __FILE__ ) );
-            define ( 'MV_TESTIMONIALS_URL', plugin_dir_url( __FILE__ ) );
-            define ( 'MV_TESTIMONIALS_VERSION', '1.0.0' );
+            define('MV_TESTIMONIALS_PATH', plugin_dir_path(__FILE__));
+            define('MV_TESTIMONIALS_URL', plugin_dir_url(__FILE__));
+            define('MV_TESTIMONIALS_VERSION', '1.0.0');
         }
 
         /**
          * Activate the plugin
          */
-        public static function activate(){
-            update_option('rewrite_rules', '' );
+        public static function activate()
+        {
+            update_option('rewrite_rules', '');
         }
 
         /**
          * Deactivate the plugin
          */
-        public static function deactivate(){
+        public static function deactivate()
+        {
+            unregister_post_type('mv-testimonials');
             flush_rewrite_rules();
         }
 
         /**
          * Uninstall the plugin
          */
-        public static function uninstall(){
-
+        public static function uninstall()
+        {
         }
-
     }
 }
 
-if( class_exists( 'MV_Testimonials' ) ){
+if (class_exists('MV_Testimonials')) {
     // Installation and uninstallation hooks
-    register_activation_hook( __FILE__, array( 'MV_Testimonials', 'activate'));
-    register_deactivation_hook( __FILE__, array( 'MV_Testimonials', 'deactivate'));
-    register_uninstall_hook( __FILE__, array( 'MV_Testimonials', 'uninstall' ) );
+    register_activation_hook(__FILE__, array('MV_Testimonials', 'activate'));
+    register_deactivation_hook(__FILE__, array('MV_Testimonials', 'deactivate'));
+    register_uninstall_hook(__FILE__, array('MV_Testimonials', 'uninstall'));
 
     $mv_testimonials = new MV_Testimonials();
 }
