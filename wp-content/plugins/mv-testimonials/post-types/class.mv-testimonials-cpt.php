bd997aa64cb7eb04ee104f154c4cd0c58f3f251f
d1av <davi4alves@gmail.com>
2023-05-30 00:26:34 -0300

    feat: :sparkles: add admin post fields

--- wp-content/plugins/mv-testimonials/post-types/class.mv-testimonials-cpt.php
+++ wp-content/plugins/mv-testimonials/post-types/class.mv-testimonials-cpt.php
@@ -6,6 +6,9 @@ if (!class_exists('MV_Testimonials_Post_Type')) {
     public function __construct()
     {
       add_action('init', array($this, 'create_post_type'));
+
+      add_action('add_meta_boxes', array($this, 'add_meta_boxes'));
+      add_action('save_post', array($this, 'save_post'));
     }
 
     public function create_post_type()
@@ -32,6 +35,58 @@ if (!class_exists('MV_Testimonials_Post_Type')) {
         'show_in_rest' => true,
         'menu_icon' => 'dashicons-testimonial',
       ));
+    }
+
+    public function add_meta_boxes()
+    {
+      add_meta_box(
+        'mv_testimonials_meta_box',
+        esc_html__('Testimonials Options', 'mv-testimonials'),
+        array($this, 'add_inner_meta_boxes'),
+        'mv-testimonials',
+        'normal',
+        'high'
+      );
+    }
+
+    public function add_inner_meta_boxes($post)
+    {
+      require_once(MV_TESTIMONIALS_PATH . 'views/mv-testimonials_metabox.php');
+    }
+
+    public function save_post($post_id)
+    {
+      if (isset($_POST['mv_testimonials_nonce'])) {
+        if (!wp_verify_nonce($_POST['mv_testimonials_nonce'], 'mv_testimonials_nonce')) {
+          return;
+        }
+      }
+
+      if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) {
+        return;
+      }
+
+      if (isset($_POST['post_type']) && $_POST['post_type'] === 'mv-testimonials') {
+        if (!current_user_can('edit_page', $post_id)) {
+          return;
+        } elseif (!current_user_can('edit_post', $post_id)) {
+          return;
+        }
+      }
+
+      if (isset($_POST['action']) && $_POST['action'] == 'editpost') {
+
+        $old_occupation = get_post_meta($post_id, 'mv_testimonials_occupation', true);
+        $new_occupation = $_POST['mv_testimonials_occupation'];
+        $old_company = get_post_meta($post_id, 'mv_testimonials_company', true);
+        $new_company = $_POST['mv_testimonials_company'];
+        $old_user_url = get_post_meta($post_id, 'mv_testimonials_user_url', true);
+        $new_user_url = $_POST['mv_testimonials_user_url'];
+
+        update_post_meta($post_id, 'mv_testimonials_occupation', sanitize_text_field($new_occupation), $old_occupation);
+        update_post_meta($post_id, 'mv_testimonials_company', sanitize_text_field($new_company), $old_company);
+        update_post_meta($post_id, 'mv_testimonials_user_url', esc_url_raw($new_user_url), $old_user_url);
+      }
     }
   }
 }
---
+++ wp-content/plugins/mv-testimonials/views/mv-testimonials_metabox.php
@@ -0,0 +1,33 @@
+<?php
+$occupation = get_post_meta($post->ID, 'mv_testimonials_occupation', true);
+$company = get_post_meta($post->ID, 'mv_testimonials_company', true);
+$user_url = get_post_meta($post->ID, 'mv_testimonials_user_url', true);
+?>
+
+<table class="form-table mv-testimonials-metabox">
+    <input type="hidden" name="mv_testimonials_nonce" value="<?= wp_create_nonce('mv_testimonials_nonce') ?>" />
+    <tr>
+        <th>
+            <label for="mv_testimonials_occupation"><?php esc_html_e('User occupation', 'mv-testimonials'); ?></label>
+        </th>
+        <td>
+            <input type="text" name="mv_testimonials_occupation" id="mv_testimonials_occupation" class="regular-text occupation" value="<?= isset($occupation) ? esc_html($occupation) : '' ?>">
+        </td>
+    </tr>
+    <tr>
+        <th>
+            <label for="mv_testimonials_company"><?php esc_html_e('User company', 'mv-testimonials'); ?></label>
+        </th>
+        <td>
+            <input type="text" name="mv_testimonials_company" id="mv_testimonials_company" class="regular-text company" value="<?= isset($company) ? esc_html($company) : '' ?>">
+        </td>
+    </tr>
+    <tr>
+        <th>
+            <label for="mv_testimonials_user_url"><?php esc_html_e('User URL', 'mv-testimonials'); ?></label>
+        </th>
+        <td>
+            <input type="url" name="mv_testimonials_user_url" id="mv_testimonials_user_url" class="regular-text user-url" value="<?= isset($user_url) ? esc_url($user_url) : '' ?>">
+        </td>
+    </tr>
+</table>
